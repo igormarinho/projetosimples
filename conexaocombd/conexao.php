875fdcd8f5ef81c28620a7a0d0d420e93f51aeab
igormarinho <igormarinhosilva@gmail.com>
2019-01-24 23:32:29 -0200

    fdf
    fdfdf

---
+++ conexaocombd/conexao.php
@@ -0,0 +1,7 @@
+<?php
+$servidor = 'localhost';
+$user = 'root';
+$password = '';
+$dbname = 'projeto';
+
+$conn = mysqli_connect($servidor,$user,$password,$dbname);
--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 var app = angular.module("app", ["ngRoute"]);
 app.config(['$routeProvider', function ($routeProvider) {
-	        $routeProvider.when('/inicial', {templateUrl: 'paginas/inicial.html'});
-	        $routeProvider.when('/pagina1', {templateUrl: 'paginas/pagina1.html' });
-	        $routeProvider.when('/pagina2', {templateUrl: 'paginas/pagina2.html'});
-            $routeProvider.when('/pagina3', {templateUrl: 'paginas/pagina3.html'});
-			$routeProvider.when('/pagina4', {templateUrl: 'paginas/pagina4.html'});
-			$routeProvider.when('/pagina5', {templateUrl: 'paginas/pagina5.html'});
-			$routeProvider.when('/unidade1', {templateUrl: 'paginas/unidade1.html'});
-			$routeProvider.when('/certificado',{templateUrl: 'paginas/certificado.html'});
+	        $routeProvider.when('/inicial', {templateUrl: 'template/inicial.html'});
+	        $routeProvider.when('/pagina1', {templateUrl: 'template/pagina1.html' });
+	        $routeProvider.when('/pagina2', {templateUrl: 'template/pagina2.html'});
+            $routeProvider.when('/pagina3', {templateUrl: 'template/pagina3.html'});
+			$routeProvider.when('/pagina4', {templateUrl: 'template/pagina4.html'});
+			$routeProvider.when('/pagina5', {templateUrl: 'template/pagina5.html'});
+			$routeProvider.when('/unidade1', {templateUrl: 'template/pagina6.html'});
+			$routeProvider.when('/certificado',{templateUrl: 'template/pagina7.html'});
 	        $routeProvider.otherwise({redirectTo: 'inicial'});
 	    }]);
 
